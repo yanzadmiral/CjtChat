4ba68ef4472a03d3cae9b758d089aa53ae49afda
Yayan Cahyani <yayancloud@gmail.com>
2020-12-14 17:45:14 +0700

    sec 26 edit data and ui inside edit profile

--- src/pages/auth/editprofile.vue
+++ src/pages/auth/editprofile.vue
@@ -6,18 +6,14 @@
             <img :src="image_url" alt="a" class="image--cover" ref="file" @click="GambarFilePicker">
         </div>
         <f7-list no-hairlines-md>
-            <f7-list-input :value="name" @input="name=$event.target.value" type="text" placeholder="Your name" clear-button>
+            <f7-list-input :value="display_name" @input="display_name=$event.target.value" type="text" placeholder="Your name" clear-button>
             </f7-list-input>
 
-            <f7-list-input :value="email" @input="email=$event.target.value" type="email" placeholder="Your email" clear-button>
-            </f7-list-input>
-
-            <f7-list-input :value="password" @input="password=$event.target.value" type="password" placeholder="Your password" clear-button>
-            </f7-list-input>
         </f7-list>
         <f7-block>
-            <f7-button outline @click="signUp">Sign Up</f7-button>
+            <f7-button outline @click="updateProfile">Update Profile</f7-button>
             <input type="file" ref="file" style="display:none;" @change="onFilePicked">
+            {{display_name}}
         </f7-block>
     </f7-page>
 </template>
@@ -34,51 +30,43 @@ export default {
        }
     },
     computed:{
+        display_name :{
+            get: function(){
+                return this.$store.getters.display_name
+            },
+            set: function(value){
+                this.$store.commit('setDisplayName',value)
+            }
+        },
         image_url(){
             return this.$store.getters.image_url
         },
         files(){
             return this.$store.getters.files
         },
-        signed_up(){
-            return this.$store.getters.signed_up
+        photo_url(){
+            return this.$store.getters.photo_url
         }
     },
     watch:{
-        signed_up(value){
-            if (value == true) {
-                this.$f7router.navigate('/signin/')
-            }
-        }
+
     },
     methods:{
-        signUp(){
-            const self = this
-            let payload = {}
-            payload.name = this.name
-            payload.password = this.password
-            payload.email = this.email
-            payload.photoURL = this.image_url
-            if (self.files) {
-                self.$store.dispatch('uploadedFile').then(url=>{
-                    payload.photoURL = url
-                    self.$store.dispatch('signUp',payload)
-                    
-                })
-            }else{
-                this.$store.dispatch('signUp',payload)
-            }
-            
-        },
         onFilePicked(){
             this.$store.dispatch('readFile')
         },
         GambarFilePicker(){
             this.$refs.file.click()
         },
+        updateProfile(){
+            const self = this
+            this.$store.commit('setAlertMessage',self.display_name);
+        }
     },
     created(){
-        this.$store.commit('setSignedUp',false)
+        if (this.photo_url != null) {
+            this.$store.commit('setImageUrl',this.photo_url)
+        }
     }
 }
 </script>
