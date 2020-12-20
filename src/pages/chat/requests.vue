6dcf86f11fbb9de3b7e2b2628d93902e5b7bb9f3
Yayan Cahyani <yayancloud@gmail.com>
2020-12-20 15:59:54 +0700

    sec 36 beauty the request

--- src/pages/chat/requests.vue
+++ src/pages/chat/requests.vue
@@ -1,15 +1,32 @@
 <template>
   <f7-page name="requests">
     <f7-navbar title="Request" back-link="Back"></f7-navbar>
-    <f7-block-title>About My Requests</f7-block-title>
-    <f7-block strong>
-      <pre>{{friend_request}}</pre>
-    </f7-block>
+    <f7-block-title>Friend Request</f7-block-title>
+      <f7-list media-list>
+      <f7-list-item v-for="(friend_requests,index) in friend_request" :key="index"
+        swipeout
+        link="#"
+        :title="friend_requests.name">
+        <img class="small-avatar" slot="media" :src="friend_requests.photoURL"/>
+        <f7-swipeout-actions right>
+          <f7-swipeout-button color="red" @click="remove(friend_requests)">Remove</f7-swipeout-button>
+          <f7-swipeout-button color="green" @click="confirm(friend_requests)">Confirm</f7-swipeout-button>
+        </f7-swipeout-actions>
+      </f7-list-item>
+    </f7-list>
   </f7-page>
 </template>
 
 <script>
 export default {
+  methods:{
+    confirm(value){
+
+    },
+    remove(value){
+      
+    }
+  },
   computed:{
     friend_request(){
       return this.$store.getters.friend_request
@@ -20,3 +37,13 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.small-avatar{
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+        object-fit: cover;
+        object-position: center;
+    }
+</style>
