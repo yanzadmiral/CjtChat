fe8cd089b2e3350fa747f3db8bf5a587e0e07278
Yayan Cahyani <yayancloud@gmail.com>
2020-12-31 18:44:15 +0700

    sec 40 beauty home page

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -21,52 +21,15 @@
     <!-- Toolbar-->
     
     <!-- Page content-->
-    <f7-block strong>
-      <p>Here is your blank Framework7 app. Let's see what we have here. </p>
-      <p></p>
-      <pre>{{ friends }}</pre>
-    </f7-block>
-    <f7-block-title>Navigation</f7-block-title>
-    <f7-list>
-      <f7-list-item link="/about/" title="About"></f7-list-item>
-      <f7-list-item link="/form/" title="Form"></f7-list-item>
+    <f7-list media-list>
+      <f7-list-item v-for="(frd,index) in friends" :key="index"
+        swipeout
+        link="#"
+        :title="frd.name">
+        <img class="small-avatar" slot="media" :src="frd.photoURL"/>
+      </f7-list-item>
     </f7-list>
 
-    <f7-block-title>Modals</f7-block-title>
-    <f7-block strong>
-      <f7-row>
-        <f7-col width="50">
-          <f7-button fill raised popup-open="#my-popup">Popup</f7-button>
-        </f7-col>
-        <f7-col width="50">
-          <f7-button fill raised login-screen-open="#my-login-screen">Login Screen</f7-button>
-        </f7-col>
-      </f7-row>
-    </f7-block>
-
-    <f7-block-title>Panels</f7-block-title>
-    <f7-block strong>
-      <f7-row>
-        <f7-col width="50">
-          <f7-button fill raised panel-open="left">Left Panel</f7-button>
-        </f7-col>
-      </f7-row>
-    </f7-block>
-
-    <f7-list>
-      <f7-list-item
-        title="Dynamic (Component) Route"
-        link="/dynamic-route/blog/45/post/125/?foo=bar#about"
-      ></f7-list-item>
-      <f7-list-item
-        title="Default Route (404)"
-        link="/load-something-that-doesnt-exist/"
-      ></f7-list-item>
-      <f7-list-item
-        title="Request Data & Load"
-        link="/request-and-load/user/123456/"
-      ></f7-list-item>
-    </f7-list>
   </f7-page>
 </template>
 
@@ -87,6 +50,12 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+  .small-avatar{
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+        object-fit: cover;
+        object-position: center;
+    }
 </style>
